4ebd2e03d6a7ad79683789c7be7d5ada6f0e0eb9
benoit bessiere <benoit.bessiere@gmail.com>
2021-09-22 15:20:43 +0200

    set I18n + home html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'calculator', to: 'pages#calculator', as: :calculator
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get 'calculator', to: 'pages#calculator', as: :calculator
+  end
 end
